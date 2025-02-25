7c520fb3750dbaa5f59ee13585226d3bb6106e15
ashirbad <ashirbadroutray847@gmail.com>
2025-02-25 07:43:45 +0530

    Add files via upload

---
+++ Willow-chatbot/main.js
@@ -0,0 +1,99 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const businessInfo = `
+
+Tone Instructions:
+Conciseness: Respond in short, informative sentences.
+Formality: Use polite language with slight formality (e.g., "Please let us know," "We are happy to assist").
+Clarity: Avoid technical jargon unless necessary.
+Consistency: Ensure responses are aligned in tone and style across all queries.
+Example: "Thank you for reaching out! Please let us know if you need further assistance."
+
+`;
+
+const API_KEY = "AIzaSyBrVgd9rHcdUoXlsDWCrs2BA3S2QxCTJEI";
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ 
+    model: "gemini-1.5-pro",
+    systemInstruction: businessInfo
+});
+
+let messages = {
+    history: [],
+}
+
+async function sendMessage() {
+
+    console.log(messages);
+    const userMessage = document.querySelector(".chat-window input").value;
+    
+    if (userMessage.length) {
+
+        try {
+            document.querySelector(".chat-window input").value = "";
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="user">
+                    <p>${userMessage}</p>
+                </div>
+            `);
+
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="loader"></div>
+            `);
+
+            const chat = model.startChat(messages);
+
+            let result = await chat.sendMessageStream(userMessage);
+            
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="model">
+                    <p></p>
+                </div>
+            `);
+            
+            let modelMessages = '';
+
+            for await (const chunk of result.stream) {
+              const chunkText = chunk.text();
+              modelMessages = document.querySelectorAll(".chat-window .chat div.model");
+              modelMessages[modelMessages.length - 1].querySelector("p").insertAdjacentHTML("beforeend",`
+                ${chunkText}
+            `);
+            }
+
+            messages.history.push({
+                role: "user",
+                parts: [{ text: userMessage }],
+            });
+
+            messages.history.push({
+                role: "model",
+                parts: [{ text: modelMessages[modelMessages.length - 1].querySelector("p").innerHTML }],
+            });
+
+        } catch (error) {
+            document.querySelector(".chat-window .chat").insertAdjacentHTML("beforeend",`
+                <div class="error">
+                    <p>The message could not be sent. Please try again.</p>
+                </div>
+            `);
+        }
+
+        document.querySelector(".chat-window .chat .loader").remove();
+        
+    }
+}
+
+document.querySelector(".chat-window .input-area button")
+.addEventListener("click", ()=>sendMessage());
+
+document.querySelector(".chat-button")
+.addEventListener("click", ()=>{
+    document.querySelector("body").classList.add("chat-open");
+});
+
+document.querySelector(".chat-window button.close")
+.addEventListener("click", ()=>{
+    document.querySelector("body").classList.remove("chat-open");
+});
+
